b42e52d601792fe34d193e86d95c50118cce7692
Blackbaer89 <d-mueller29@t-online.de>
2024-10-21 10:08:42 +0200

    added input field

---
+++ lib/widgets/HpTextField.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HpTextField extends StatelessWidget {
+  const HpTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField();
+  }
+}
