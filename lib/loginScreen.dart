05e8b7a43d6fbd77cbab6da3d5a03ee61a21cb0d
Blackbaer89 <d-mueller29@t-online.de>
2024-10-21 10:09:40 +0200

    added login screen

---
+++ lib/loginScreen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 207, 222, 243),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Dein Logo hier
+                Image.asset('assets/images/happy_place.png'),
+                const SizedBox(height: 20),
+                const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Benutzername oder E-Mail',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Passwort',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Passwort vergessen?'),
+                ),
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Log in'),
+                ),
+                const SizedBox(height: 20),
+                const Text('Oder'),
+                const SizedBox(height: 20),
+                // Buttons für Google, Facebook und Apple-Login
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Google Login Button
+                    // ...
+                    // Facebook Login Button
+                    // ...
+                    // Apple Login Button
+                    // ...
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
