944ea4c0a562c96b91bf3b63468cf40afcc751dd
Blackbaer89 <d-mueller29@t-online.de>
2024-12-10 10:28:57 +0100

    implement Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:happy_pleace/feature/authentication/screens/login_screen.dart';
+import 'package:happy_pleace/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
