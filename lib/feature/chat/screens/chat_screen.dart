3c23c59d2bbbdee47d9c784236636730c5750898
Blackbaer89 <d-mueller29@t-online.de>
2025-02-17 09:30:55 +0100

    new chat

--- lib/feature/chat/screens/chat_screen.dart
+++ lib/feature/chat/screens/chat_screen.dart
@@ -1,67 +1,151 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:happy_place/config/colors.dart';
-import 'package:happy_place/feature/chat/widgets/chatContainer.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    if (text.isNotEmpty) {
+      setState(() {
+        _messages.add(ChatMessage(text: text, isUser: true));
+        _textController.clear();
+      });
+
+      Future.delayed(const Duration(milliseconds: 500), () {
+        final random = Random();
+        final responses = [
+          "HeyHey😊 Mir geht's gut! Wie geht's? \nWas machst du so?",
+          "Hallo! Mir geht es prima, danke. Und dir?",
+          "Hi! Alles bestens bei mir. Was gibt's Neues \nbei dir?",
+          "Hey! Ich bin gut drauf. Wie sieht's bei dir aus?",
+        ];
+        final randomIndex = random.nextInt(responses.length);
+        final botResponse = responses[randomIndex];
+
+        setState(() {
+          _messages.add(ChatMessage(text: botResponse, isUser: false));
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: hpBlue,
-        appBar: AppBar(
-          backgroundColor: shadowBlue,
-          title: const Text(
-            'Chats',
-            style: TextStyle(color: hpwhite),
-          ),
+      backgroundColor: hpBlue,
+      appBar: AppBar(
+        backgroundColor: shadowBlue,
+        title: const Text(
+          'Chats',
+          style: TextStyle(color: hpwhite),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ChatContainer(),
-                SizedBox(
-                  height: 32,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hey wie gehts dir?',
-                      style: TextStyle(fontSize: 16),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: shadowBlue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Row(
+                children: [
+                  CircleAvatar(
+                      radius: 20,
+                      backgroundImage:
+                          AssetImage('assets/profilimage/Anna.png')),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Text(
+                      'Anna Schlosser',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: hpwhite,
+                          fontWeight: FontWeight.w500),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text('Gut und selbst?', style: TextStyle(fontSize: 16)),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                        'Schlechten Leuten geht es immer gut 🤣 .\nHast du heute schon was vor?',
-                        style: TextStyle(fontSize: 16)),
-                  ],
-                ),
-                Spacer(),
-                TextField(
-                  decoration: InputDecoration(hintText: 'Nachricht schreiben'),
-                ),
-              ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              ),
+            ),
+            _buildTextComposer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration(hintText: 'Nachricht schreiben'),
             ),
           ),
-        ));
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () => _handleSubmitted(_textController.text),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final bool isUser;
+
+  const ChatMessage({required this.text, required this.isUser, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment:
+            isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: isUser ? Colors.blue : Colors.grey[300],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                color: isUser ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
